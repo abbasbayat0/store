73e8c2d9aba31e4ceeda2c4f77aaec83c64a2d1c
abbasbayat0 <abbas0bayat@gmail.com>
2025-08-31 10:38:58 +0330

    D: carousel functionality

--- src/components/shared/home/HeroCarousel.tsx
+++ src/components/shared/home/HeroCarousel.tsx
@@ -5,25 +5,24 @@ const HeroCarousel = () => {
   const list = [0, 1, 2];
   const [active, setActive] = useState(0);
   const situation = active === 0 ? 'first' : active === list.length - 1 ? 'last' : '';
-  const handlePrev = () => {
-    const newActive = (active - 1 + list.length) % list.length;
-    setActive(newActive);
-  };
-  const handleNext = () => {
-    const newActive = (active + 1) % list.length;
-    setActive(newActive);
-  };
+  // const handlePrev = () => {
+  //   const newActive = (active - 1 + list.length) % list.length;
+  //   setActive(newActive);
+  // };
+  // const handleNext = () => {
+  //   const newActive = (active + 1) % list.length;
+  //   setActive(newActive);
+  // };
   return (
-    <div className='relative h-96 w-1/2 hidden lg:flex'>
-      <div className='flex h-96 w-full items-center justify-center'>
+    <div className='relative hidden h-96 w-1/2 lg:flex'>
+      <div className='flex h-96 w-full items-center justify-center bg-red-300'>
         {list.map((item) => {
-          const isThisActive = item === active;
           const goLeft = item < active;
           const goRight = item > active;
           return (
             <div key={item} className='flex items-center justify-center'>
               <div
-                className={`absolute flex h-11/12 w-11/12 items-center justify-center bg-green-300 ${!isThisActive && 'hidden'}`}
+                className={`absolute flex h-11/12 min-w-11/12 items-center justify-center bg-green-300 ${goRight && 'left-[110%]'} ${goLeft && '-left-[110%]'} ${!goLeft && !goRight && 'left-5'} transition-all duration-1000 ease-out`}
               >
                 {item}
               </div>
@@ -33,14 +32,14 @@ const HeroCarousel = () => {
         <button
           disabled={situation === 'first'}
           className={`absolute left-0 cursor-pointer disabled:opacity-40`}
-          onClick={() => handlePrev()}
+          onClick={() => setActive(active - 1)}
         >
           prev
         </button>
         <button
           disabled={situation === 'last'}
           className={`cursor-pointe absolute right-0 disabled:opacity-40`}
-          onClick={() => handleNext()}
+          onClick={() => setActive(active + 1)}
         >
           next
         </button>
