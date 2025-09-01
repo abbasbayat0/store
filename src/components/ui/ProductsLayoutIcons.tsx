2ec8122c3002da765b350c4d2a99b7d995459155
abbasbayat0 <abbas0bayat@gmail.com>
2025-09-02 00:00:58 +0330

    D: grid or list switch

--- src/components/ui/ProductsLayoutIcons.tsx
+++ src/components/ui/ProductsLayoutIcons.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { RootState } from '@/lib/store/store';
+import Link from 'next/link';
 import { LuLayoutGrid, LuList } from 'react-icons/lu';
 import { useSelector } from 'react-redux';
 
@@ -7,12 +8,16 @@ const ProductsLayoutIcons = ({ layout, searchTerms }: { layout: string; searchTe
   const dark = useSelector((state: RootState) => state.theme.dark);
   return (
     <div className='flex gap-5'>
-      <LuLayoutGrid
-        className={`h-8 w-8 rounded-md p-2 ${layout === 'grid' ? `bg-blue-600 ${dark ? 'text-black' : 'text-white'}` : `${dark && 'text-white'}`}`}
-      />
-      <LuList
-        className={`h-8 w-8 rounded-md p-2 ${layout === 'list' ? `bg-blue-600 ${dark ? 'text-black' : 'text-white'}` : `${dark && 'text-white'}`}`}
-      />
+      <Link href={`/products?layout=grid${searchTerms}`}>
+        <LuLayoutGrid
+          className={`h-8 w-8 rounded-md p-2 ${layout === 'grid' ? `bg-blue-600 ${dark ? 'text-black' : 'text-white'}` : `${dark && 'text-white'}`}`}
+        />
+      </Link>
+      <Link href={`/products?layout=list${searchTerms}`}>
+        <LuList
+          className={`h-8 w-8 rounded-md p-2 ${layout === 'list' ? `bg-blue-600 ${dark ? 'text-black' : 'text-white'}` : `${dark && 'text-white'}`}`}
+        />
+      </Link>
     </div>
   );
 };
