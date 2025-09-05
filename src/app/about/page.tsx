1b2a76b0b0e21e6148a68e9bb7b9647b531c03fd
abbasbayat0 <abbas0bayat@gmail.com>
2025-09-06 00:44:56 +0330

    D: resolve about page bug

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,11 @@
 import About from '@/components/ui/About';
 
 const page = () => {
-  return;
-  <div className='min-h-screen'>
-    <About />
-  </div>;
+  return (
+    <div className='min-h-screen'>
+      <About />
+    </div>
+  );
 };
 
 export default page;
--- src/components/ui/About.tsx
+++ src/components/ui/About.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { RootState } from '@/lib/store/store';
 import { useSelector } from 'react-redux';
 
