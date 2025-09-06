43babb85511799803f5166fdeff23ff1bffd0a5b
abbasbayat0 <abbas0bayat@gmail.com>
2025-09-06 12:45:52 +0330

    D: dropdown links transition

--- src/components/ui/LinksDropDown.tsx
+++ src/components/ui/LinksDropDown.tsx
@@ -9,20 +9,20 @@ const DarkModeDropdown = ({ setDropOpen }: { setDropOpen: Dispatch<SetStateActio
   const dark = useSelector((state: RootState) => state.theme.dark);
   return (
     <div
-      className={`absolute top-9 left-0 flex w-36 flex-col items-start justify-around gap-1 rounded-md px-2 py-2 sm:w-28 lg:w-32 xl:w-36 ${dark ? 'border border-gray-800 bg-gray-950' : 'border-none bg-white shadow-md shadow-gray-300'}`}
+      className={`absolute top-9 left-0 flex w-36 flex-col items-start justify-around gap-1 rounded-md px-2 py-2 sm:w-28 lg:w-32 xl:w-36 ${dark ? 'border border-gray-800 bg-gray-950' : 'border-none bg-white shadow-md shadow-gray-300'} transition duration-300`}
     >
       {links.map((link) => {
         return (
           <div
             key={link.label}
-            className={`w-full border-b ${dark ? 'border-gray-800' : 'border-gray-100'}`}
+            className={`w-full border-b ${dark ? 'border-gray-800' : 'border-gray-100'} transition duration-300`}
           >
             <Link
               href={link.href}
               onClick={() => {
                 setDropOpen(false);
               }}
-              className={`flex h-7 w-full items-center rounded-md px-2 tracking-wide opacity-80 transition ${dark ? 'hover:bg-gray-800' : 'hover:bg-gray-100'}`}
+              className={`flex h-7 w-full items-center rounded-md px-2 tracking-wide opacity-80 transition ${dark ? 'hover:bg-gray-800' : 'hover:bg-gray-100'} transition duration-300`}
             >
               {link.label}
             </Link>
