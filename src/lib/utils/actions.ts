183e40644a2d8154140f3a5efb87a1582030ea9a
abbasbayat0 <abbas0bayat@gmail.com>
2025-09-06 12:22:23 +0330

    optimize the actions

--- src/lib/utils/actions.ts
+++ src/lib/utils/actions.ts
@@ -2,35 +2,76 @@
 
 import { redirect } from 'next/navigation';
 import db from './prisma';
+import { Product } from '@prisma/client';
+import { unstable_cache } from 'next/cache';
 
-export const getFeatured = async () => {
-  const featured = await db.product.findMany({
-    where: { featured: true },
-  });
-  return featured;
-};
+export const getFeatured = unstable_cache(
+  async () => {
+    let data: Product[] = [];
+    let message = '';
+    try {
+      data = await db.product.findMany({
+        where: { featured: true },
+      });
+      message = `success, you have ${data.length} products`;
+    } catch (error) {
+      message = `failed because of ${error}`;
+    } finally {
+      await db.$disconnect();
+    }
+    return { message, data };
+  },
+  ['featured'],
+  { tags: ['featured'], revalidate: 1800 },
+);
 
-export const getAll = async (search: string = '') => {
-  const all = await db.product.findMany({
-    where: {
-      OR: [
-        { name: { contains: search, mode: 'insensitive' } },
-        { company: { contains: search, mode: 'insensitive' } },
-      ],
-    },
-    orderBy: {
-      createdAt: 'desc',
-    },
-  });
-  return all;
-};
+export const getAll = unstable_cache(
+  async (search: string = '') => {
+    let data: Product[] = [];
+    let message = '';
+    try {
+      data = await db.product.findMany({
+        where: {
+          OR: [
+            { name: { contains: search, mode: 'insensitive' } },
+            { company: { contains: search, mode: 'insensitive' } },
+          ],
+        },
+        orderBy: {
+          createdAt: 'desc',
+        },
+      });
+      message = `success, you have ${data.length} products`;
+    } catch (error) {
+      message = `failed because of ${error}`;
+    } finally {
+      await db.$disconnect();
+    }
+    return { message, data };
+  },
+  ['all'],
+  { tags: ['all'], revalidate: 1800 },
+);
 
-export const getSingle = async (id: string) => {
-  const product = await db.product.findUnique({
-    where: {
-      id: id,
-    },
-  });
-  if (!product) redirect('/products');
-  return product;
-};
+export const getSingle = unstable_cache(
+  async (id: string) => {
+    let data: Product | null = null;
+    let message = '';
+    try {
+      data = await db.product.findUnique({
+        where: {
+          id: id,
+        },
+      });
+      message = `product with id:${id} now available in data`;
+    } catch (error) {
+      message = `failed because of ${error}`;
+    } finally {
+      await db.$disconnect();
+      if (!data) redirect('/products');
+    }
+    return { message, data };
+  },
+  ['unique'],
+  { tags: ['unique'], revalidate: 1800 },
+);
