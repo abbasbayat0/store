51498b736f1573840483b1dfd4b68a43dac055c9
abbasbayat0 <abbas0bayat@gmail.com>
2025-09-09 17:59:08 +0330

    supabase is so idiot

--- src/lib/utils/actions.ts
+++ src/lib/utils/actions.ts
@@ -5,9 +5,6 @@ import db from './prisma';
 import { Product } from '@prisma/client';
 import { unstable_cache } from 'next/cache';
 
-import { revalidatePath } from 'next/cache';
-import { createClient } from '@/lib/supabase/server';
-
 export const getFeatured = unstable_cache(
   async () => {
     let data: Product[] = [];
@@ -78,43 +75,3 @@ export const getSingle = unstable_cache(
   ['unique'],
   { tags: ['unique'], revalidate: 1800 },
 );
-
-export async function login(formData: FormData) {
-  const supabase = await createClient();
-
-  // type-casting here for convenience
-  // in practice, you should validate your inputs
-  const data = {
-    email: formData.get('email') as string,
-    password: formData.get('password') as string,
-  };
-
-  const { error } = await supabase.auth.signInWithPassword(data);
-
-  if (error) {
-    redirect('/error');
-  }
-
-  revalidatePath('/', 'layout');
-  redirect('/');
-}
-
-export async function signup(formData: FormData) {
-  const supabase = await createClient();
-
-  // type-casting here for convenience
-  // in practice, you should validate your inputs
-  const data = {
-    email: formData.get('email') as string,
-    password: formData.get('password') as string,
-  };
-
-  const { error } = await supabase.auth.signUp(data);
-
-  if (error) {
-    redirect('/error');
-  }
-
-  revalidatePath('/', 'layout');
-  redirect('/');
-}
