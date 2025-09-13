22ca3c28b56b148cd603b6ea8f2848f4d6ca36ef
abbasbayat0 <abbas0bayat@gmail.com>
2025-09-13 10:21:42 +0330

    make product detail better

--- src/app/admin/Sidebar.tsx
+++ src/app/admin/Sidebar.tsx
@@ -1,12 +1,16 @@
+'use client';
 import { adminLinks } from '@/assets/links';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 const AdminSidebar = () => {
+  const pathName = usePathname();
   return (
     <div className='flex flex-col'>
       {adminLinks.map((link) => {
+        const isActive = pathName === link.href;
         return (
-          <Link href={link.href} key={link.href}>
+          <Link href={link.href} key={link.href} className={`${isActive && 'bg-blue-600'}`}>
             {link.label}
           </Link>
         );
--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,31 +1,36 @@
 import BreadCrumbs from '@/components/shared/singleProduct/BreadCrumbs';
 import Description from '@/components/shared/singleProduct/Description';
 import { getSingle } from '@/lib/utils/actions';
+import EmptyList from '@/components/shared/global/EmptyList';
 import Image from 'next/image';
 
 const page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const { id } = await params;
   const { message, data } = await getSingle(id);
-  if (!data) console.log(message);
-  return (
-    <div className='min-h-screen'>
-      <BreadCrumbs name={data.name} />
-      <div className='lg:flex lg:gap-16'>
-        <div className='mt-16 hidden min-h-screen min-w-5/12 overflow-hidden rounded-lg lg:mt-5 lg:flex'>
-          <Image
-            width={300}
-            height={400}
-            priority
-            unoptimized
-            src={data.image}
-            alt={id}
-            className='min-h-full min-w-full object-cover'
-          />
+  if (!data) {
+    console.log(message);
+    return <EmptyList className='mt-20' text='Item Not Found' />;
+  }
+  if (data)
+    return (
+      <div className='min-h-screen'>
+        <BreadCrumbs name={data.name} />
+        <div className='lg:flex lg:gap-16'>
+          <div className='mt-16 hidden min-h-screen min-w-5/12 overflow-hidden rounded-lg lg:mt-5 lg:flex'>
+            <Image
+              width={300}
+              height={400}
+              priority
+              unoptimized
+              src={data.image}
+              alt={id}
+              className='min-h-full min-w-full object-cover'
+            />
+          </div>
+          <Description {...data} />
         </div>
-        <Description {...data} />
       </div>
-    </div>
-  );
+    );
 };
 
 export default page;
