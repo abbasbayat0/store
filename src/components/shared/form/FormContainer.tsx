70f785e4d2d3313e07e6f181ca40898a77d31287
abbasbayat0 <abbas0bayat@gmail.com>
2025-09-15 15:07:53 +0330

    form container is now available

---
+++ src/components/shared/form/FormContainer.tsx
@@ -0,0 +1,30 @@
+'use client';
+import { actionFunction } from '@/lib/types/formTypes';
+import { useEffect } from 'react';
+import { useFormState } from 'react-dom';
+
+const FormContainer = ({
+  action,
+  children,
+}: {
+  action: actionFunction;
+  children: React.ReactNode;
+}) => {
+  const initialState = {
+    message: '',
+  };
+  const [state, formAction] = useFormState(action, initialState);
+  useEffect(() => {
+    console.log(state.message);
+  }, [state]);
+  return (
+    <form
+      action={formAction}
+      className={`mt-5 flex flex-col rounded-md border border-gray-300 p-8 shadow-md transition duration-300 dark:border-gray-800`}
+    >
+      {children}
+    </form>
+  );
+};
+
+export default FormContainer;
