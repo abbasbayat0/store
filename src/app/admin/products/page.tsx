bf5fe9e571a29ad0af36e7606e6dc55235b4cb7e
abbasbayat0 <abbas0bayat@gmail.com>
2025-09-24 23:45:58 +0330

    render admin products

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -9,7 +9,33 @@ const page = async () => {
   }
   return (
     <div className='min-h-screen'>
-      <div></div>
+      <header className='w-full border-b border-gray-300 pb-3'>
+        <div className='flex justify-around'>
+          <p className='font-semibold opacity-70 dark:text-white'>Name</p>
+          <p className='font-semibold opacity-70 dark:text-white'>Company</p>
+          <p className='font-semibold opacity-70 dark:text-white'>Price</p>
+          <p className='font-semibold opacity-70 dark:text-white'>Actions</p>
+        </div>
+      </header>
+      <main className='mt-5 flex flex-col items-center justify-center gap-3'>
+        {data.map((product) => {
+          return (
+            <div
+              key={product.id}
+              className='mt-2 flex w-full items-center justify-around border-b pb-1 border-gray-300'
+            >
+              <p className='font-semibold dark:text-white min-w-1/4 text-center'>{product.name}</p>
+              <p className='font-semibold dark:text-white min-w-1/4 text-center'>{product.company}</p>
+              <p className='font-semibold dark:text-white min-w-1/4 text-center'>{product.price}</p>
+              <div className='flex gap-2 min-w-1/4 justify-center items-center'>
+                <p>1</p>
+                <p>2</p>
+              </div>
+            </div>
+          );
+        })}
+      </main>
+      <footer></footer>
     </div>
   );
 };
