8133362927d2266e11bed33fe054ded140a4c5a1
abbasbayat0 <abbas0bayat@gmail.com>
2025-09-27 15:28:32 +0330

    create get admin

--- src/lib/utils/getUser.ts
+++ src/lib/utils/getUser.ts
@@ -5,3 +5,8 @@ export const getUser = async () => {
   if (!user) throw new Error('you are not a user');
   return user;
 };
+export const getAdmin = async () => {
+  const user = await getUser();
+  if (user.id !== process.env.NEXT_PUBLIC_ADMIN_ID) throw new Error('you are not admin');
+  return user;
+};
