de6f42c2c1e9c14dd19f652d4b7dd0c23ea6e808
abbasbayat0 <abbas0bayat@gmail.com>
2025-09-28 02:37:40 +0330

    edit link is correct now, delete button do not need to on click

--- src/components/shared/form/AdminProductsButtons.tsx
+++ src/components/shared/form/AdminProductsButtons.tsx
@@ -1,36 +1,37 @@
-'use client';
-import { useFormStatus } from 'react-dom';
 import { GrRefresh } from 'react-icons/gr';
 import { CiEdit } from 'react-icons/ci';
 import { LuTrash2 } from 'react-icons/lu';
-import { deleteProduct } from '@/lib/utils/actions';
 import Link from 'next/link';
 
-const AdminProductsButtons = ({ type, id }: { type: 'edit' | 'delete'; id: string }) => {
-  const { pending } = useFormStatus();
+const AdminProductsButtons = ({
+  type,
+  id,
+  pending,
+}: {
+  type: 'edit' | 'delete';
+  id: string;
+  pending?: boolean;
+}) => {
   const buttonIcon = () => {
     switch (type) {
       case 'edit':
         return (
-          <Link href={`/admin/create/${id}`}>
+          <Link href={`/admin/products/${id}`}>
             <CiEdit className='cursor-pointer text-lg transition duration-300 hover:scale-110 dark:text-white' />
           </Link>
         );
       case 'delete':
         return (
-          <LuTrash2
-            className='cursor-pointer text-lg opacity-80 transition duration-300 hover:scale-110 dark:text-white'
-            onClick={() => {
-              deleteProduct(id);
-            }}
-          />
+          <LuTrash2 className='cursor-pointer text-lg opacity-80 transition duration-300 hover:scale-110 dark:text-white' />
         );
       default:
         const never: never = type;
         throw new Error(`invalid type ${never}`);
     }
   };
-  return <button>{pending ? <GrRefresh className='animate-spin' /> : buttonIcon()}</button>;
+  return (
+    <button type='submit'>{pending ? <GrRefresh className='animate-spin' /> : buttonIcon()}</button>
+  );
 };
 
 export default AdminProductsButtons;
