1743491fac0fe2690d1871d3c88bc703c460dc03
abbasbayat0 <abbas0bayat@gmail.com>
2025-10-01 16:49:36 +0330

    working on form

--- src/components/shared/products/FavoriteToggleForm.tsx
+++ src/components/shared/products/FavoriteToggleForm.tsx
@@ -1,5 +1,22 @@
+'use client';
+import { useFormStatus } from 'react-dom';
+import { IoReloadCircleOutline } from 'react-icons/io5';
+
 const FavoriteToggleForm = () => {
-  return <div>FavoriteToggleForm</div>;
+  const { pending } = useFormStatus();
+  return (
+    <form>
+      <button type='submit' className='cursor-pointer p-2'>
+        {pending ? (
+          <IoReloadCircleOutline className='animate-spin' />
+        ) : isFavorite ? (
+          <FaHeart />
+        ) : (
+          <FaRegHeart />
+        )}
+      </button>
+    </form>
+  );
 };
 
 export default FavoriteToggleForm;
