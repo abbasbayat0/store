85ebcdfb6b9e07996b85c18ee55d60813144160c
abbasbayat0 <abbas0bayat@gmail.com>
2025-10-01 16:49:29 +0330

    working on button

--- src/components/shared/products/FavoriteToggleButton.tsx
+++ src/components/shared/products/FavoriteToggleButton.tsx
@@ -1,12 +1,23 @@
+import { SignInButton } from '@clerk/nextjs';
+import { auth } from '@clerk/nextjs/server';
 import { CiHeart } from 'react-icons/ci';
+import FavoriteToggleForm from './FavoriteToggleForm';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const FavoriteToggleButton = ({ id }: { id: string }) => {
+const FavoriteToggleButton = async ({ id }: { id: string }) => {
+  const { userId } = await auth();
+  let user = 
+  if(userId)
   return (
     <div
       className={`z-10 flex h-8 w-8 cursor-pointer items-center justify-center rounded-md bg-gray-100 p-1 text-black transition duration-300 hover:bg-gray-200 dark:bg-gray-800 dark:text-white dark:hover:bg-gray-700`}
     >
-      <CiHeart className='text-3xl' />
+      {userId ? (
+        <FavoriteToggleForm />
+      ) : (
+        <SignInButton>
+          <CiHeart className='text-3xl' />
+        </SignInButton>
+      )}
     </div>
   );
 };
