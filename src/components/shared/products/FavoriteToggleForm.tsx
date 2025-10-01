5cdde26231b6df125f8ea02f95e5d8c108126257
abbasbayat0 <abbas0bayat@gmail.com>
2025-10-01 17:12:19 +0330

    working on form for favorite

--- src/components/shared/products/FavoriteToggleForm.tsx
+++ src/components/shared/products/FavoriteToggleForm.tsx
@@ -1,11 +1,24 @@
 'use client';
-import { useFormStatus } from 'react-dom';
+import { toggleFavoriteAction } from '@/lib/utils/actions';
+import { usePathname } from 'next/navigation';
+import { useActionState } from 'react';
+import { FaHeart } from 'react-icons/fa';
+import { FaRegHeart } from 'react-icons/fa';
 import { IoReloadCircleOutline } from 'react-icons/io5';
 
-const FavoriteToggleForm = () => {
-  const { pending } = useFormStatus();
+const FavoriteToggleForm = ({
+  productId,
+  favoriteId,
+}: {
+  productId: string;
+  favoriteId: string | null;
+}) => {
+  const pathName = usePathname();
+  const action = toggleFavoriteAction.bind(null, { productId, favoriteId, pathName });
+  const [state, formAction, pending] = useActionState(action, { message: '' });
+  const isFavorite = favoriteId ? true : false;
   return (
-    <form>
+    <form action={formAction}>
       <button type='submit' className='cursor-pointer p-2'>
         {pending ? (
           <IoReloadCircleOutline className='animate-spin' />
