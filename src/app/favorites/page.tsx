81f4b190b4cf2c0d48e48f8d0074992c16f140c1
abbasbayat0 <abbas0bayat@gmail.com>
2025-10-04 13:46:17 +0330

    favorite page render

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -1,8 +1,28 @@
+import LoadingContainer from '@/components/shared/global/LoadingContainer';
+import SectionTitle from '@/components/shared/global/SectionTitle';
+import Cart from '@/components/ui/Cart';
 import { fetchFavorites } from '@/lib/utils/actions';
+import { Suspense } from 'react';
 
 const page = async () => {
   const favoriteProducts = await fetchFavorites();
-  return <p className='dark:text-white'>you have {favoriteProducts?.length} items</p>;
+  return (
+    <div>
+      <SectionTitle title='Favorites' />
+      <Suspense fallback={<LoadingContainer />}>
+        {favoriteProducts?.map((product) => {
+          return (
+            <div
+              key={product.id}
+              className='mt-5 flex flex-wrap items-center justify-center gap-10'
+            >
+              <Cart {...product.product} />
+            </div>
+          );
+        })}
+      </Suspense>
+    </div>
+  );
 };
 
 export default page;
