1c128dda9ec069d46f70b6bdf812b0379547a9c4
abbasbayat0 <abbas0bayat@gmail.com>
2025-10-05 13:58:03 +0330

    better looking skeleton

--- src/components/shared/global/LoadingContainer.tsx
+++ src/components/shared/global/LoadingContainer.tsx
@@ -2,7 +2,7 @@ import CartSkeleton from '@/components/ui/CartSkeleton';
 
 const LoadingContainer = () => {
   return (
-    <div className='mt-5 flex min-h-screen flex-wrap items-center justify-center gap-10 lg:-mt-40'>
+    <div className='mt-5 flex min-h-screen flex-wrap items-center justify-center gap-10 lg:-mt-32'>
       <CartSkeleton />
       <CartSkeleton />
       <CartSkeleton />
