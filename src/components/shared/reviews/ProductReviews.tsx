350be12cf32af3ae78a5c6fc1668fc5cfb36dbc0
abbasbayat0 <abbas0bayat@gmail.com>
2025-11-01 13:19:59 +0330

    get data in product review

--- src/components/shared/reviews/ProductReviews.tsx
+++ src/components/shared/reviews/ProductReviews.tsx
@@ -1,8 +1,25 @@
-import { fetchProductRating } from '@/lib/utils/actions';
+import { fetchProductReviews } from '@/lib/utils/actions';
+import ReviewCard from './ReviewCard';
+import SectionTitle from '../global/SectionTitle';
 
 const ProductReviews = async ({ productId }: { productId: string }) => {
-  const { message, data } = await fetchProductRating(productId);
-  return <div>ProductReviews</div>;
+  const { message, data } = await fetchProductReviews(productId);
+  if (message !== 'successfully received')
+    return (
+      <div className='mt-10 flex items-center justify-center'>
+        <p>There Is No Comment For This Product</p>
+      </div>
+    );
+  return (
+    <div className='mt-20'>
+      <SectionTitle title='Product Reviews' />
+      <div className='mt-10 flex flex-wrap gap-8 justify-around items-center'>
+        {data.map((item, i) => {
+          return <ReviewCard key={i} {...item} />;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default ProductReviews;
