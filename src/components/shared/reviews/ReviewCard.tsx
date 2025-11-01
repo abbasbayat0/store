8a3733ae327d02f030adbba3844be582f4e215d7
abbasbayat0 <abbas0bayat@gmail.com>
2025-11-01 13:20:19 +0330

    render data

--- src/components/shared/reviews/ReviewCard.tsx
+++ src/components/shared/reviews/ReviewCard.tsx
@@ -1,5 +1,28 @@
-const ReviewCard = () => {
-  return <div>ReviewCard</div>;
+import Image from 'next/image';
+import Comment from './Comment';
+
+const ReviewCard = ({
+  rating,
+  comment,
+  authorImageUrl,
+  authorName,
+}: {
+  rating: number;
+  comment: string;
+  authorImageUrl: string;
+  authorName: string;
+}) => {
+  return (
+    <div className='min-w-5/12'>
+      <div>
+        <div className='w-10 h-10 rounded-full relative overflow-hidden'>
+          <Image src={authorImageUrl} alt={authorName} fill className='object-cover' />
+        </div>
+        <div></div>
+      </div>
+      <Comment />
+    </div>
+  );
 };
 
 export default ReviewCard;
