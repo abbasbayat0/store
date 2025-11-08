6da4e3ae8bd4c051884d2adadb6b8b0a41cb7932
abbasbayat0 <abbas0bayat@gmail.com>
2025-11-08 11:06:40 +0330

    styling the review card

--- src/components/shared/reviews/ReviewCard.tsx
+++ src/components/shared/reviews/ReviewCard.tsx
@@ -1,26 +1,32 @@
 import Image from 'next/image';
 import Comment from './Comment';
+import { fetchProductRatingByProductId } from '@/lib/utils/actions';
+import Rating from './Rating';
 
-const ReviewCard = ({
-  rating,
+const ReviewCard = async ({
   comment,
   authorImageUrl,
   authorName,
 }: {
-  rating: number;
   comment: string;
   authorImageUrl: string;
   authorName: string;
 }) => {
+  const { message, data } = await fetchProductRatingByProductId(authorName);
+  console.log(message);
+  const sendingShape = [data as { rating: number }];
   return (
-    <div className='min-w-5/12'>
-      <div>
-        <div className='w-10 h-10 rounded-full relative overflow-hidden'>
+    <div className='min-w-5/12 rounded-xl border border-gray-100 p-5 shadow-md'>
+      <div className='flex items-center gap-3'>
+        <div className='relative h-10 w-10 overflow-hidden rounded-full'>
           <Image src={authorImageUrl} alt={authorName} fill className='object-cover' />
         </div>
-        <div></div>
+        <div className='flex flex-col gap-1'>
+          <p className='text-sm dark:text-white'>{authorName}</p>
+          <Rating data={sendingShape} />
+        </div>
       </div>
-      <Comment />
+      <Comment comment={comment} />
     </div>
   );
 };
