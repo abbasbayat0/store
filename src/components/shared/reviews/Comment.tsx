3c78a1511bedb9fe33a3b87d1ac4a7b92d0b8461
abbasbayat0 <abbas0bayat@gmail.com>
2025-11-08 11:05:43 +0330

    comments of reviews is fully completed

--- src/components/shared/reviews/Comment.tsx
+++ src/components/shared/reviews/Comment.tsx
@@ -1,5 +1,30 @@
-const Comment = () => {
-  return <div>Comment</div>;
+'use client';
+
+import { useState } from 'react';
+
+const Comment = ({ comment }: { comment: string }) => {
+  const [show, setShow] = useState(false);
+
+  const finalComment = show ? comment : comment.slice(0, 50);
+
+  return (
+    <div className='mt-5 text-sm opacity-80 dark:text-white'>
+      {comment.length <= 120 ? (
+        <p>{finalComment}</p>
+      ) : (
+        <p>
+          {finalComment} ...
+          <br />
+          <span
+            onClick={() => setShow(!show)}
+            className={`mx-2 cursor-pointer text-sm opacity-80 transition duration-300 hover:text-blue-600 hover:opacity-100 dark:hover:text-blue-400`}
+          >
+            {show ? 'Show Less' : 'Show More'}
+          </span>
+        </p>
+      )}
+    </div>
+  );
 };
 
 export default Comment;
