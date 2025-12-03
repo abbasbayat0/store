ed2a416f65283318d8b240f29b603d01bca665c2
ABBAS <abbas0bayat@gmail.com>
2025-12-03 16:04:13 +0330

    create three column of cartItems

--- src/components/shared/cart/CartItemColumns.tsx
+++ src/components/shared/cart/CartItemColumns.tsx
@@ -1,5 +1,39 @@
-const CartItemColumns = () => {
-  return <div>CartItemColumns</div>;
+import { formatCurrency } from '@/lib/utils/format';
+import Image from 'next/image';
+import Link from 'next/link';
+export const FirstColumn = ({ name, image }: { image: string; name: string }) => {
+  return (
+    <div className='relative h-24 w-24 sm:h-32 sm:w-32'>
+      <Image
+        src={image}
+        alt={name}
+        fill
+        sizes='(max-width:768px) 100vw,(max-width:1200px) 50vw,33vw'
+        priority
+        className='w-full rounded-md object-cover'
+      />
+    </div>
+  );
+};
+export const SecondColumn = ({
+  name,
+  company,
+  productId,
+}: {
+  name: string;
+  company: string;
+  productId: string;
+}) => {
+  return (
+    <div className='sm:w-48'>
+      <Link href={`/products/${productId}`}>
+        <h3 className='font-medium capitalize hover:underline'>{name}</h3>
+      </Link>
+      <h4 className='mt-2 text-xs capitalize'>{company}</h4>
+    </div>
+  );
 };
 
-export default CartItemColumns;
+export const FourthColumn = ({ price }: { price: number }) => {
+  return <p className='font-medium md:ml-auto'>{formatCurrency(price)}</p>;
+};
